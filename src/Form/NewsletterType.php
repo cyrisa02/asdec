211da6326e2b044f53629f5a71fd00025c9a2b13
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-09-20 18:08:51 +0200

    Add Send Email for Newsletter

---
+++ src/Form/NewsletterType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Newsletter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class NewsletterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '190',
+                ],
+                'label' => 'Titre',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    
+                ],
+                'label' => 'Message',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ]
+            ])
+            
+           
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Newsletter::class,
+        ]);
+    }
+}
